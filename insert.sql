4c1494e7e94344f22b24cd235d9f87d06aa5e41e
Pedro Jofre Lora <pjofrelora@gmail.com>
2018-11-16 11:56:56 -0500

    Done

---
+++ insert.sql
@@ -0,0 +1,1 @@
+INSERT INTO planets (name, color, num_of_moons, mass, rings) VALUES ('Mercury', 'gray', 0, 0.55, 0), ('Venus', 'yellow', 0, 0.82, 0), ('Earth', 'blue', 1, 1.0, 0), ('Mars', 'red', 2, 0.11, 0), ('Jupiter', 'orange', 53, 317.9, 0), ('Saturn', 'hazel', 62, 95.19, 1), ('Uranus', 'dark blue', 27, 14.54, 1), ('Neptune', 'light blue', 14, 17.15, 1), ('Pluto', 'brown', 5, 0.003, 0);
